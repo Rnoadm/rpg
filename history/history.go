98e11764e819ce833f185fe48851d56eed2b0022
Ben Lubar <ben.lubar@gmail.com>
2014-10-06 13:42:54 -0500

    history

---
+++ history/history.go
@@ -0,0 +1,195 @@
+package history
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/gob"
+	"github.com/BenLubar/bindiff"
+	"github.com/Rnoadm/rpg"
+	"io"
+)
+
+const (
+	SeekStart = iota
+	SeekCur
+	SeekEnd
+)
+
+const sizeof_int64 = 64 / 8
+
+type History struct {
+	i int64
+	b []byte
+	f io.ReadWriteSeeker
+}
+
+func NewHistory(f io.ReadWriteSeeker) *History {
+	return &History{f: f, i: -1}
+}
+
+func (h *History) Seek(offset int64, whence int) (*rpg.State, error) {
+	if whence == SeekStart || h.i < 0 {
+		if _, err := h.f.Seek(0, SeekStart); err != nil {
+			return nil, err
+		}
+		h.i, h.b = -1, nil
+		if err := h.seekForward(); err != nil {
+			return nil, err
+		}
+	}
+	if whence == SeekEnd {
+		for {
+			err := h.seekForward()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	for offset > 0 {
+		if err := h.seekForward(); err != nil {
+			return nil, err
+		}
+		offset--
+	}
+	for offset < 0 {
+		if err := h.seekReverse(); err != nil {
+			return nil, err
+		}
+		offset++
+	}
+
+	var s *rpg.State
+	err := gob.NewDecoder(bytes.NewReader(h.b)).Decode(&s)
+	if err == io.EOF {
+		err = io.ErrUnexpectedEOF
+	}
+	return s, err
+}
+
+func (h *History) seekForward() error {
+	var size int64
+	err := binary.Read(h.f, binary.LittleEndian, &size)
+	if err != nil {
+		return err
+	}
+
+	patch := make([]byte, size)
+	_, err = io.ReadFull(h.f, patch)
+	if err != nil {
+		if err == io.EOF {
+			err = io.ErrUnexpectedEOF
+		}
+		return err
+	}
+
+	_, err = h.f.Seek(sizeof_int64, SeekCur)
+	if err != nil {
+		if err == io.EOF {
+			err = io.ErrUnexpectedEOF
+		}
+		return err
+	}
+
+	h.i++
+
+	h.b, err = bindiff.Forward(h.b, patch)
+
+	return err
+}
+
+func (h *History) seekReverse() error {
+	if h.i == 0 {
+		return io.EOF
+	}
+
+	_, err := h.f.Seek(-sizeof_int64, SeekCur)
+	if err != nil {
+		if err == io.EOF {
+			err = io.ErrUnexpectedEOF
+		}
+		return err
+	}
+
+	var size int64
+	err = binary.Read(h.f, binary.LittleEndian, &size)
+	if err != nil {
+		if err == io.EOF {
+			err = io.ErrUnexpectedEOF
+		}
+		return err
+	}
+
+	_, err = h.f.Seek(-size-sizeof_int64*2, SeekCur)
+	if err != nil {
+		if err == io.EOF {
+			err = io.ErrUnexpectedEOF
+		}
+		return err
+	}
+
+	patch := make([]byte, sizeof_int64+size)
+	_, err = io.ReadFull(h.f, patch)
+	if err != nil {
+		if err == io.EOF {
+			err = io.ErrUnexpectedEOF
+		}
+		return err
+	}
+
+	h.i--
+
+	h.b, err = bindiff.Reverse(h.b, patch[sizeof_int64:])
+
+	return err
+}
+
+func (h *History) Append(s *rpg.State) error {
+	_, err := h.Seek(0, SeekEnd)
+	first := false
+	if err == io.EOF && h.i == -1 {
+		first, err = true, nil
+	}
+	if err != nil {
+		return err
+	}
+
+	var buf bytes.Buffer
+	err = gob.NewEncoder(&buf).Encode(s)
+	if err != nil {
+		return err
+	}
+
+	patch := bindiff.Diff(h.b, buf.Bytes(), 10)
+
+	if !first {
+		h.i++
+	}
+	h.b = buf.Bytes()
+
+	err = binary.Write(h.f, binary.LittleEndian, int64(len(patch)))
+	if err != nil {
+		return err
+	}
+
+	n, err := h.f.Write(patch)
+	if err != nil {
+		return err
+	}
+	if n != len(patch) {
+		return io.ErrShortWrite
+	}
+
+	err = binary.Write(h.f, binary.LittleEndian, int64(len(patch)))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (h *History) Tell() int64 {
+	return h.i
+}
---
+++ history/history_test.go
@@ -0,0 +1,220 @@
+package history
+
+import (
+	"bytes"
+	"github.com/Rnoadm/rpg"
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func testSetup0(t *testing.T) (*History, func()) {
+	f, err := ioutil.TempFile(os.TempDir(), "testhistory")
+	if err != nil {
+		t.Error(err)
+		t.SkipNow()
+	}
+	return NewHistory(f), func() {
+		n := f.Name()
+		f.Close()
+		os.Remove(n)
+	}
+}
+
+func testSetup1(t *testing.T) (*History, []byte, func()) {
+	h, cleanup := testSetup0(t)
+
+	s := rpg.NewState()
+	err := h.Append(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	g0, err := s.GobEncode()
+	if err != nil {
+		t.Error(err)
+	}
+
+	return h, g0, cleanup
+}
+
+func testSetup2(t *testing.T) (*History, []byte, []byte, func()) {
+	h, cleanup := testSetup0(t)
+
+	s := rpg.NewState()
+	err := h.Append(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	g0, err := s.GobEncode()
+	if err != nil {
+		t.Error(err)
+	}
+	if !s.Atomic(func(s *rpg.State) bool {
+		_, _ = s.Create()
+		return true
+	}) {
+		t.Fatal("Atomic failed")
+	}
+	err = h.Append(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	g1, err := s.GobEncode()
+	if err != nil {
+		t.Error(err)
+	}
+
+	return h, g0, g1, cleanup
+}
+
+func expect(t *testing.T, state []byte, expected error, tell int64) func(*rpg.State, error) func(int64) {
+	return func(s *rpg.State, err error) func(int64) {
+		if err != expected {
+			t.Error("not equal: ", err, " != ", expected)
+		}
+		if (s == nil) != (state == nil) {
+			t.Error("not equal: ", s, " != ", state)
+		} else if state != nil {
+			b, err := s.GobEncode()
+			if err != nil {
+				t.Error(err)
+			}
+			if !bytes.Equal(b, state) {
+				t.Error("not equal: ", b, " != ", state)
+			}
+		}
+
+		return func(i int64) {
+			if i != tell {
+				t.Error("not equal: ", i, " != ", tell)
+			}
+		}
+	}
+}
+
+func TestSeekPrevEmpty(t *testing.T) {
+	h, cleanup := testSetup0(t)
+	defer cleanup()
+
+	expect(t, nil, io.EOF, -1)(h.Seek(-1, SeekCur))
+}
+
+func TestSeekCurEmpty(t *testing.T) {
+	h, cleanup := testSetup0(t)
+	defer cleanup()
+
+	expect(t, nil, io.EOF, -1)(h.Seek(0, SeekCur))
+}
+
+func TestSeekFirstEmpty(t *testing.T) {
+	h, cleanup := testSetup0(t)
+	defer cleanup()
+
+	expect(t, nil, io.EOF, -1)(h.Seek(0, SeekStart))
+}
+
+func TestSeekSecondEmpty(t *testing.T) {
+	h, cleanup := testSetup0(t)
+	defer cleanup()
+
+	expect(t, nil, io.EOF, -1)(h.Seek(1, SeekStart))
+}
+
+func TestSeekSecondLastEmpty(t *testing.T) {
+	h, cleanup := testSetup0(t)
+	defer cleanup()
+
+	expect(t, nil, io.EOF, -1)(h.Seek(-1, SeekEnd))
+}
+
+func TestSeekLastEmpty(t *testing.T) {
+	h, cleanup := testSetup0(t)
+	defer cleanup()
+
+	expect(t, nil, io.EOF, -1)(h.Seek(0, SeekEnd))
+}
+
+func TestSeekPrevOne(t *testing.T) {
+	h, _, cleanup := testSetup1(t)
+	defer cleanup()
+
+	expect(t, nil, io.EOF, -1)(h.Seek(-1, SeekCur))
+}
+
+func TestSeekCurOne(t *testing.T) {
+	h, b0, cleanup := testSetup1(t)
+	defer cleanup()
+
+	expect(t, b0, nil, 0)(h.Seek(0, SeekCur))(h.Tell())
+}
+
+func TestSeekFirstOne(t *testing.T) {
+	h, b0, cleanup := testSetup1(t)
+	defer cleanup()
+
+	expect(t, b0, nil, 0)(h.Seek(0, SeekStart))(h.Tell())
+}
+
+func TestSeekSecondOne(t *testing.T) {
+	h, _, cleanup := testSetup1(t)
+	defer cleanup()
+
+	expect(t, nil, io.EOF, -1)(h.Seek(1, SeekStart))
+}
+
+func TestSeekSecondLastOne(t *testing.T) {
+	h, _, cleanup := testSetup1(t)
+	defer cleanup()
+
+	expect(t, nil, io.EOF, -1)(h.Seek(-1, SeekEnd))
+}
+
+func TestSeekLastOne(t *testing.T) {
+	h, b0, cleanup := testSetup1(t)
+	defer cleanup()
+
+	expect(t, b0, nil, 0)(h.Seek(0, SeekEnd))(h.Tell())
+}
+
+func TestSeekPrevTwo(t *testing.T) {
+	h, b0, _, cleanup := testSetup2(t)
+	defer cleanup()
+
+	expect(t, b0, nil, 0)(h.Seek(-1, SeekCur))(h.Tell())
+}
+
+func TestSeekCurTwo(t *testing.T) {
+	h, _, b1, cleanup := testSetup2(t)
+	defer cleanup()
+
+	expect(t, b1, nil, 1)(h.Seek(0, SeekCur))(h.Tell())
+}
+
+func TestSeekFirstTwo(t *testing.T) {
+	h, b0, _, cleanup := testSetup2(t)
+	defer cleanup()
+
+	expect(t, b0, nil, 0)(h.Seek(0, SeekStart))(h.Tell())
+}
+
+func TestSeekSecondTwo(t *testing.T) {
+	h, _, b1, cleanup := testSetup2(t)
+	defer cleanup()
+
+	expect(t, b1, nil, 1)(h.Seek(1, SeekStart))(h.Tell())
+}
+
+func TestSeekSecondLastTwo(t *testing.T) {
+	h, b0, _, cleanup := testSetup2(t)
+	defer cleanup()
+
+	expect(t, b0, nil, 0)(h.Seek(-1, SeekEnd))(h.Tell())
+}
+
+func TestSeekLastTwo(t *testing.T) {
+	h, _, b1, cleanup := testSetup2(t)
+	defer cleanup()
+
+	expect(t, b1, nil, 1)(h.Seek(0, SeekEnd))(h.Tell())
+}
